f0cbe2f78aac5b69e3239ae143281c40169807b1
Leonardo Silveira <sombriks@gmail.com>
2017-06-11 16:01:50 -0300

    ainda sem samber por que o demo quebra, mas vejo isso um dia.

--- src/features/S0007-lancamento/lancamento.vue
+++ src/features/S0007-lancamento/lancamento.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <div class="row center-xs top-xs">
-      <div class="col-xs-11">
+      <div class="col-xs-8 col-xs-offset-2">
         <h1 class="r">Lançamentos</h1>
+        <p>Papo rápido. Quanto foi, quando foi e, opcionalmente, uma descrição.</p>
       </div>
     </div>
   </div>
